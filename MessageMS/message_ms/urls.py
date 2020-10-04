a52cc1761159e02852c659a11da17f21dadb36ad
Diana Guarin <dcguarina@unal.edu.co>
2020-10-03 19:11:00 -0500

    commit

--- MessageMS/message_ms/urls.py
+++ MessageMS/message_ms/urls.py
@@ -1,7 +1,6 @@
 from django.urls import path
 from rest_framework.urlpatterns import format_suffix_patterns
 from message_ms.views.conversation_view import ConversationList
-from message_ms.views.conversation_view import ConversationDetail
 from message_ms.views.message_view import MessageList
 from message_ms.views.message_view import MessageDetail
 
--- MessageMS/message_ms/views/__init__.py
+++ MessageMS/message_ms/views/__init__.py
@@ -1,3 +1,2 @@
 from .conversation_view import ConversationList
-from .conversation_view import ConversationDetail
 from .message_view import MessageList
