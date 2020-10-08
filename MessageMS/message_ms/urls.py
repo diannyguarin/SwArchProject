76edf64de607af04a6656795418a287f0c4317a7
Diana Guarin <dcguarina@unal.edu.co>
2020-10-07 19:23:48 -0500

    commit 7

--- MessageMS/message_ms/urls.py
+++ MessageMS/message_ms/urls.py
@@ -1,13 +1,14 @@
 from django.urls import path
 from rest_framework.urlpatterns import format_suffix_patterns
 from message_ms.views.conversation_view import ConversationList
+from message_ms.views.conversation_view import ConversationDetail
 from message_ms.views.message_view import MessageList
 from message_ms.views.message_view import MessageDetail
 
 urlpatterns = [
     path('conversations/<int:pk1>', ConversationList.conversation_list),
-    path('conversations/<int:pk1>/<int:pk2>', ConversationList.conversation_messages),
-    path('conversations/<int:pk1>/<int:pk2>/<int:pk3>', ConversationList.messages_delete),
+    path('conversations<int:pk1>/<int:pk2>', MessageList.as_view()),
+    path('conversations/<int:pk1>/<int:pk2>/<int:pk3>', MessageDetail.as_view()),
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
