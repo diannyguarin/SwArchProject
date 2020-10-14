0c01e07c8dd1375a5c09fb863b29a0d9fdc9ebca
Diana Guarin <dcguarina@unal.edu.co>
2020-10-13 19:58:50 -0500

    message graphiQL fixes

--- DayscoolAPI/src/dayscool/messages/typeDefs.js
+++ DayscoolAPI/src/dayscool/messages/typeDefs.js
@@ -1,5 +1,5 @@
 // Definir el objeto que van a usar y los campos necesarios para crearlo
-import { GraphQLInt, GraphQLNonNull } from 'graphql'
+import { GraphQLInt, GraphQLNonNull, GraphQLSchema } from 'graphql'
 const conversation = new GraphQLObjectType({ name: 'conversation', fields: { id: { type: new GraphQLNonNull(GraphQLInt) }, usuario1Id: { type: new GraphQLNonNull(GraphQLInt) }, usuario2Id: { type: new GraphQLNonNull(GraphQLInt) }, }, })
 export const conversationTypeDef = `
   type Conversation {
