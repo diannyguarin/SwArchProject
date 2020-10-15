6f2c36d7baed8bc282fe936423a38d3df46c41ba
Diana Guarin <dcguarina@unal.edu.co>
2020-10-14 22:07:40 -0500

    Update typeDefs.js

--- DayscoolAPI/src/dayscool/messages/typeDefs.js
+++ DayscoolAPI/src/dayscool/messages/typeDefs.js
@@ -1,6 +1,4 @@
-// Definir el objeto que van a usar y los campos necesarios para crearlo
-import { GraphQLInt, GraphQLNonNull } from 'graphql'
-const conversation = new GraphQLObjectType({ name: 'conversation', fields: { id: { type: new GraphQLNonNull(GraphQLInt) }, usuario1Id: { type: new GraphQLNonNull(GraphQLInt) }, usuario2Id: { type: new GraphQLNonNull(GraphQLInt) }, }, })
+
 export const conversationTypeDef = `
   type Conversation {
       id: Int!
